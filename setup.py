6fb1fd6ee2b7a9d32d6d052fa13d2115a374f208
Junya Hayashi <ledmonster@gmail.com>
2013-08-08 18:04:57 +0900

    Implemented setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='unga',
+      version='0.1',
+      description='Twisted based messaging service',
+      packages = ['unga'],
+      license='MIT',
+      author='Junya Hayashi',
+      install_requires=[
+          "Twisted>=13.1.0",
+      ]
+)
