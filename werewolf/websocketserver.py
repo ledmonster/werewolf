04c70c581cbda50bd0fbe46cd3c2858d4e8442df
Junya Hayashi <ledmonster@gmail.com>
2013-09-21 19:18:27 +0900

    Now, websocket server can get access_token and detect user

--- werewolf/websocketserver.py
+++ werewolf/websocketserver.py
@@ -1,20 +1,27 @@
 """ websocket server """
 from tornado import websocket, web, ioloop
 
-cl = []
+from werewolf.models import *
+
+clients = []
 
 class SocketHandler(websocket.WebSocketHandler):
 
+    user = None
+
     def open(self):
-        if self not in cl:
-            cl.append(self)
+        token = self.get_argument('access_token')
+        self.user = AccessToken.objects.get(token=token).client_session.user
+        if self not in clients:
+            clients.append(self)
 
     def on_message(self, message):
-        self.write_message(u"You said: " + message)
+        for client in clients:
+            client.write_message(self.user.name + u" said: " + message)
 
     def on_close(self):
-        if self in cl:
-            cl.remove(self)
+        if self in clients:
+            clients.remove(self)
 
 
 if __name__ == '__main__':
