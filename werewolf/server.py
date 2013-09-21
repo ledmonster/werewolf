20be55cee0304c216a8c9b633075298dba6a6e1a
Junya Hayashi <ledmonster@gmail.com>
2013-09-21 18:31:39 +0900

    use Tornado instead of Twisted

--- werewolf/server.py
+++ werewolf/server.py
@@ -4,14 +4,22 @@ import os
 # initialize django
 os.environ["DJANGO_SETTINGS_MODULE"] = "werewolf.settings"
 
-from twisted.internet import reactor
-from werewolf.apiserver import wsgi_app
-from werewolf.streamserver import stream_app
+from tornado.web import Application, FallbackHandler
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from tornado.httpserver import HTTPServer
+
+from werewolf.apiserver import app as flask_app
+from werewolf.websocketserver import SocketHandler
 
 def main():
-    reactor.listenTCP(1025, stream_app)
-    reactor.listenTCP(8000, wsgi_app)
-    reactor.run()
+    app = Application([
+        (r'/websocket', SocketHandler),
+        ('.*', FallbackHandler, dict(fallback=WSGIContainer(flask_app))),
+    ])
+    http_server = HTTPServer(app)
+    http_server.listen(8000)
+    IOLoop.instance().start()
 
 if __name__ == "__main__":
     main()
