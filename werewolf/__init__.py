1d78a056696ec7d44f375c5cd0071638db869c54
Junya Hayashi <ledmonster@gmail.com>
2013-11-24 21:40:12 +0900

    Configure logging

---
+++ werewolf/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+console_log = logging.StreamHandler()
+console_log.setLevel(logging.INFO)
+console_log.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+
+logger.addHandler(console_log)
