2c5a71a6f0c712a8bca372822448c1922c5528a3
Junya Hayashi <ledmonster@gmail.com>
2014-06-03 19:18:23 +0900

    Added test for verify id_token

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
 
 setup(name='werewolf',
       version='0.1',
       description='Jinro backend and CLI',
-      packages = ['werewolf'],
+      packages=['werewolf'],
       license='MIT',
       author='Junya Hayashi',
       install_requires=[
           "tornado",
-          "Django>=1.6b4",
+          "Django>=1.6",
           "django-extensions",
           "Flask",
           "google-api-python-client",
@@ -18,7 +27,6 @@ setup(name='werewolf',
           "PIL",
           "PyYAML",
       ],
-      dependency_links=[
-          "https://github.com/django/django/archive/1.6b4.zip#egg=Django-1.6b4",
-      ]
+      tests_require=["pytest"],
+      cmdclass = {'test': PyTest},
 )
