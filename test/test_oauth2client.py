efc598573ebee19e5181844c09e9224f7fa69005
Junya Hayashi <ledmonster@gmail.com>
2014-06-03 19:18:23 +0900

    Added test for verify id_token

---
+++ test/test_oauth2client.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+
+# initialize django
+os.environ["DJANGO_SETTINGS_MODULE"] = "werewolf.settings"
+
+from werewolf import settings
+from werewolf.auth import IdTokenAuthenticator
+
+
+class TestIdTokenAuthenticator(object):
+
+    def test_validate(self):
+        client_id = settings.OAUTH2["CLIENT_ID"]
+        id_token = self.get_id_token()
+        authenticator = IdTokenAuthenticator(client_id)
+        authenticator.validate(id_token)
+
+    def get_id_token(self):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), ".id_token")
+        with open(path) as fp:
+            id_token = fp.read()
+        return id_token
