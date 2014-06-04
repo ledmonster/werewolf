46e5941a06239539bb3ef58e11c18ce1e2200f86
Junya Hayashi <ledmonster@gmail.com>
2014-06-04 14:49:54 +0900

    Changed to village based session management

--- werewolf/websocketserver.py
+++ werewolf/websocketserver.py
@@ -1,27 +1,31 @@
 """ websocket server """
 from tornado import websocket, web, ioloop
+from flask import jsonify
 
 from werewolf.models import *
 
-clients = []
+clients = {}
 
 class SocketHandler(websocket.WebSocketHandler):
 
     user = None
+    village_id = None
 
     def open(self):
         token = self.get_argument('access_token')
+        self.village_id = self.get_argument('village_id')
         self.user = AccessToken.objects.get(token=token).client_session.user
-        if self not in clients:
-            clients.append(self)
+        village_clients = clients.setdefault(self.village_id, [])
+        if self not in village_clients:
+            clients[self.village_id].append(self)
 
     def on_message(self, message):
-        for client in clients:
-            client.write_message(self.user.name + u" said: " + message)
+        for client in clients[self.village_id]:
+            client.write_message(self.user.name + u": " + message)
 
     def on_close(self):
-        if self in clients:
-            clients.remove(self)
+        if self in clients[self.village_id]:
+            clients[self.village_id].remove(self)
 
 
 if __name__ == '__main__':
