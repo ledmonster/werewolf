cefcba03a435735290b807d23cf20b38bf129803
Junya Hayashi <ledmonster@gmail.com>
2014-06-05 11:10:42 +0900

    Changed mysql password

--- werewolf/settings.py
+++ werewolf/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'werewolf',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'gree',
         'HOST': '',
         'PORT': '',
     }
