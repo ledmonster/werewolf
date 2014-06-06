2802fe4fe51f8b681a25ccae30c513e0e0d0a5f6
Junya Hayashi <ledmonster@gmail.com>
2014-06-06 13:15:00 +0900

    Updated comments

--- werewolf/application/service/__init__.py
+++ werewolf/application/service/__init__.py
@@ -1,3 +1,6 @@
-""" service on application layer """
+"""
+## NOT IN USE Now !! ##
+service on application layer
+"""
 from .game_master import ResidentManager, GameMaster
 from .messenger import Messenger
