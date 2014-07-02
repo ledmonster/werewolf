85e5d94d48ff2097d0102133141341ac93ec6af8
Junya Hayashi <ledmonster@gmail.com>
2014-07-02 21:34:42 +0900

    Changed settings.py for Heroku

--- werewolf/settings.py
+++ werewolf/settings.py
@@ -98,3 +98,27 @@ OAUTH2 = {
     "CLIENT_ID": "793850702446.apps.googleusercontent.com",
     "CLIENT_SECRET": "kenMp-cauTxqqQSPdCJff5Ob"
 }
+
+#------------------------------
+# for Heroku
+#------------------------------
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
