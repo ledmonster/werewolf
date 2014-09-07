3c2276bfd215eac61a39c9ac454fdd0eac26ff32
Junya Hayashi <ledmonster@gmail.com>
2014-09-07 14:38:13 +0900

    Remove unused googleplus logic (already exists in layout.html)

--- werewolf/app/static/scripts/view/top.js
+++ werewolf/app/static/scripts/view/top.js
@@ -12,18 +12,6 @@ namespace('werewolf.view.top', function(ns) {
 
             initialize: function() {
 
-                /**
-                 * initialize google plus
-                 */
-                function initializeGooglePlus() {
-                    var po = document.createElement('script');
-                    po.type = 'text/javascript';
-                    po.async = true;
-                    po.src = 'https://plus.google.com/js/client:plusone.js';
-                    var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(po, s);
-                }
-
                 this.onLoad()
                     .doAction(function(params) {
                         router.loadView('header', {title: 'みんなの人狼'});
