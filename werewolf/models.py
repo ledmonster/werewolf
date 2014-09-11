3d1649f378e4bd5b4f1a2e11ec713a42f9a18cea
Junya Hayashi <junya_hayashi@xica.net>
2014-09-11 15:30:04 +0900

    Remove unnecessary werewolf.models (used in Django)

--- werewolf/models.py
+++
@@ -1,4 +0,0 @@
-from werewolf.domain.user.models import User, UserCredential, ClientSession, \
-    AccessToken, RefreshToken
-from werewolf.domain.game.models import VillageModel, ResidentModel, \
-    BehaviorModel, EventModel
