5c762061a654ad2f025525b698c73fc299d616f1
Junya Hayashi <ledmonster@gmail.com>
2014-09-13 16:52:40 +0900

    Add MorningEvent on models

--- werewolf/domain/game/models/__init__.py
+++ werewolf/domain/game/models/__init__.py
@@ -2,5 +2,5 @@ from .village import VillageModel, ResidentModel, BehaviorModel,\
     Role, VillageStatus, ResidentStatus, BehaviorType, Winner
 from .event_model import EventModel, EventType
 from .event import Event, EternalEvent, MessageEvent, JoinEvent, LeaveEvent,\
-    GameStartEvent, GameEndEvent, GameResetEvent, NightEvent
+    GameStartEvent, GameEndEvent, GameResetEvent, NightEvent, MorningEvent
 from .game import GameService
